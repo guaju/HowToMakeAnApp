55c065d0762755af6610992c676cbb240e99814c
guaju <guaju@126.com>
2017-07-20 09:54:07 +0800

    Initial commit

---
+++ app/src/main/java/com/guaju/howtomakeanapp/MainActivity.java
@@ -0,0 +1,81 @@
+package com.guaju.howtomakeanapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+/* 如何去开发一款app
+   1.这个app是做什么的？
+     {
+     一、是一个电商、购物网站的app
+
+         0、注册、登录
+         1、商品的展示
+         2、排序
+         3、动画
+         4、缓存
+         5、支付
+         6、定位
+         7、订单信息
+         8、聊天
+         9、版本更新
+         10、应用发布
+
+     二、新闻资讯类的
+         0、注册、登录
+         1、新闻分类展示（Fragment嵌套Fragment来展示分类新闻）
+         2、点赞，收藏，评论
+         3、个人中心（签到，积分，积分兑换，vip）
+         4、版本更新
+         5、应用发布
+     三、O2O(美团外卖、快方、叮当快药，滴滴打人，uber)
+         0、登录注册（短信验证码）
+         1、商品展示（分类）
+         2、购物车
+         3、支付
+         4、定位，地图
+         5、聊天客服/评论、催单、投诉等
+         6、推送
+         7、个人中心（积分、优惠券、兑换、vip）
+         8、版本更新
+         9、应用发布
+      四、金融（理财产品）
+          0、登录注册（短信验证码）
+          1、商品展示（分类）（自定义控件）
+          2、支付（银联或者别的银行的，一般不会有支付宝、微信）
+          4、定位
+          5、聊天客服
+          6、推送（少部分会有）
+          7、个人中心（积分、优惠券、兑换、vip）
+          8、加密
+          9、版本更新
+          10、应用发布
+
+     五、智能硬件（连接一款设备：手环、XX检测器、XX遥控器）
+          0、登录注册（少有）
+          1、蓝牙、红外、或者wifi连接硬件设备
+          2、使用java调用c/c++（jni技术）调用硬件提供的方法（c/c++）(ndk)
+          3、拿到硬件获得的数据，进行展示，然后做出相应的操作
+          9、版本更新
+          10、应用发布
+     六、直播项目
+         核心点：推流拉流，弹幕，礼品赠送
+
+
+     }
+
+
+
+
+ */
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+
+
+
+    }
+}
